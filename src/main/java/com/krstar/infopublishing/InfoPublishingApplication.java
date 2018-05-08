6bafd974c46b10131e169f62f30ae6bdbb437a9d
casual_ji <casual_ji@hotmail.com>
2018-05-08 21:43:38 +0800

    maven package

--- src/main/java/com/krstar/infopublishing/InfoPublishingApplication.java
+++ src/main/java/com/krstar/infopublishing/InfoPublishingApplication.java
@@ -2,11 +2,18 @@ package com.krstar.infopublishing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class InfoPublishingApplication {
+public class InfoPublishingApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(InfoPublishingApplication.class);
+	}
 
 	public static void main(String[] args) {
 
